f98f06364e6147a05e667541b7500b0606679efb
lizen123 <lulu.nixon404@gmail.com>
2021-10-31 14:30:44 +0600

    first commitTt

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config()
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 // mongo user : lizen123
 // pass: ffAl5syTOpc8TQiO
 
